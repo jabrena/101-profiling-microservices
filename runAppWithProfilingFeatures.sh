91b6ef23d2dc5d1c8d4ce5e7549e3b2fca165a3b
Juan Antonio Breña Moral <bren@juanantonio.info>
2018-03-04 11:30:57 +0000

    Adding JMeter Script Adding Bash Script to run the whole process

---
+++ runAppWithProfilingFeatures.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+echo "Script"
+
+# Regenerate Jar with latest changes
+./gradlew clean compileJava bootJar
+
+set JAVA_OPTS=""
+
+# Run Jar
+java -Xms256m -Xmx256m \
+-Dcom.sun.management.jmxremote=true \
+-Dcom.sun.management.jmxremote.port=7091 \
+-Dcom.sun.management.jmxremote.authenticate=false \
+-Dcom.sun.management.jmxremote.ssl=false \
+-server \
+-jar ./build/libs/101-profiling-microservices-0.1.0-SNAPSHOT.jar
+
+
