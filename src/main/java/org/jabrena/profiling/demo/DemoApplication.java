6d8b00ee0a7f78aca4d24c115b90d87d551debec
Juan Antonio Breña Moral <bren@juanantonio.info>
2018-03-04 11:30:05 +0000

    Adding initial example

---
+++ src/main/java/org/jabrena/profiling/demo/DemoApplication.java
@@ -0,0 +1,47 @@
+package org.jabrena.profiling.demo;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@SpringBootApplication
+public class DemoApplication {
+
+	private List<String> evilList = new ArrayList<>();
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping("/demo")
+	public ResponseEntity<?> greeting() {
+
+		evilList.add(RandomStringUtils.randomAlphabetic(100));
+
+		final ResponseDemo result = new ResponseDemo("Hello World");
+		final HttpStatus status = HttpStatus.OK;
+
+		return new ResponseEntity<>(result, status);
+	}
+
+	private class ResponseDemo {
+
+		final String message;
+
+		public ResponseDemo(String message) {
+			this.message = message;
+		}
+
+		public String getMessage() {
+			return message;
+		}
+	}
+}
