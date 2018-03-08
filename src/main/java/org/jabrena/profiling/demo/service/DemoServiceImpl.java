ebd7a718171feceee5b6e3d147c6e46010632d30
Juan Antonio Breña Moral <bren@juanantonio.info>
2018-03-08 16:02:19 +0000

    Stable solution for the list

--- src/main/java/org/jabrena/profiling/demo/service/DemoServiceImpl.java
+++ src/main/java/org/jabrena/profiling/demo/service/DemoServiceImpl.java
@@ -5,27 +5,34 @@ import org.jabrena.profiling.demo.model.DataContainer;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 public class DemoServiceImpl implements DemoService {
 
+    List<DataContainer> evilList = Collections.synchronizedList(new ArrayList<DataContainer>());;
 
     @Override
     public DataContainer getToken() {
         return smartSeedComputing();
     }
 
+
+    /**
+     * A 'SMART' algorithm to provide security in the project.
+     */
     private DataContainer smartSeedComputing(){
 
-        List<DataContainer> evilList = new ArrayList<>();
 
         for(int i = 1; i <= 10; i++) {
             evilList.add(new DataContainer(RandomStringUtils.randomAlphabetic(100)));
         }
 
-        //int index = ThreadLocalRandom.current().nextInt(1, evilList.size());
-        return evilList.get(0);
+        int randomNum = ThreadLocalRandom.current().nextInt((evilList.size() - 1) + 1);
+        return evilList.get(randomNum);
     }
+
 }
